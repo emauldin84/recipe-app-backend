008e86085f062b190f86d1e944621d67d9059db3
Eric Mauldin <emauldin84@gmail.com>
2019-08-07 12:21:53 -0400

    initial commit. includes schema.sql, seed.sql and beginning backend structure

---
+++ schema.sql
@@ -0,0 +1,16 @@
+create table users (
+    id serial primary key,
+    first_name varchar(200),
+    last_name varchar(200),
+    email varchar(100),
+    user_password varchar(200)
+);
+
+create table recipes (
+    id serial primary key,
+    recipe_title varchar(300),
+    recipe_added_date date,
+    recipe_details text,
+    recipe_photos text,
+    user_id integer references users(id)
+);
