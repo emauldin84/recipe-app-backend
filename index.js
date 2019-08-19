7a6ec1b3cd53d5b67e3ae913b75e9803cdbb4f39
Eric Mauldin <emauldin84@gmail.com>
2019-08-19 10:35:45 -0400

    updates seed to formatted recipes details

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 const port = process.env.PORT
 const path = require('path')
 const cors = require('cors')
+// const bodyParser= require('body-parser')
+// const multer = require('multer');
 
 const recipesRouter = require('./routes/recipes')
 
@@ -13,9 +15,30 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
+// const storage = multer.diskStorage({
+//     destination: './public/uploads',
+//     filename: function (req, file, cb) {
+//         cb(null, 'IMAGE-' + Date.now() + path.extname(file.originalname))
+//     }
+// })
+
+// const upload = multer({
+//     storage: storage,
+//     limits: {fileSize: 10000000},
+// }).single('recipeImage')
+
+
 
 app.use('/recipes', recipesRouter)
 
+// recipesRouter.post('/upload', function(req, res) {
+//     upload(req, res, function (err) {
+//         console.log('Request ---', req.body)
+//         console.log('Request file ---', req.file)
+//         if(err)
+//         return res.send(200).end()
+//     })
+// })
 
 app.listen(port), () => {
     console.log(`Server listening on port ${port}`)
